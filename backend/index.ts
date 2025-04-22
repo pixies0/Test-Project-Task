12752b8c195901bacf08e2947da7ef1d79605b56
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-04-22 19:20:29 -0300

    :ambulance: Installing n config cors to avoid browsers blocks

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import scrapeRoute from "./routes/scrape";
+import cors from "cors";
 
 // Inicialização da aplicação Express junto com a definição da porta
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use("/api/scrape", scrapeRoute);
 
 // Inicia o servidor e escuta na porta definida
